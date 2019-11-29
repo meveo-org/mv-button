0ab0da03e23532c590ac79266810734f2024af1f
Tony Alejandro <tonysviews@gmail.com>
2019-11-29 18:50:55 +0800

    added sample action for demo

--- demo.js
+++ demo.js
@@ -4,7 +4,7 @@ import "./mv-button.js";
 export class MvButtonDemo extends LitElement {
   static get properties() {
     return {
-      value: { type: String, attribute: true }
+      value: { type: Number, attribute: false, reflect: false }
     };
   }
 
@@ -14,13 +14,54 @@ export class MvButtonDemo extends LitElement {
         font-family: var(--font-family, Arial);
         font-size: var(--font-size-m, 10pt);
       }
+
+      mv-button {
+        --mv-button-padding: 10px 12px;
+      }
+
+      .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;        
+      }
+
+      .label {
+        font-size: 20px;
+      }
+
+      .value {
+        font-size: 30px;
+      }
     `;
+  }
+
+  constructor() {
+    super();
+    this.value = 0;
   }
 
   render() {
     return html`
-    <mv-button>Test</mv-button>
+    <div class="container">
+      <mv-button
+        @button-clicked="${this.changeValue(-1)}"
+        ?disabled="${this.value < 1}"
+      >
+        <div class="label">-</div>
+      </mv-button>
+      <div class="value">Current value: ${this.value}</div>
+      <mv-button @button-clicked="${this.changeValue(1)}">
+        <div class="label">+</div>
+      </mv-button>
+    </div>
     `;
+  }
+
+  changeValue(increment) {
+    return () => {
+      this.value = this.value + increment;
+    };
   }
 }
 
